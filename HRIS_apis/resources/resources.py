e949da2af86d371fc7c3a5248b5ad36361ac86e7
maazhussaini <skr2570@gmail.com>
2024-08-01 21:17:31 +0200

    testing

--- HRIS_apis/resources/resources.py
+++ HRIS_apis/resources/resources.py
@@ -1504,9 +1504,10 @@ class IARResource(Resource):
                 else:
                     print("NOT FOUND 2")
                     db.session.add(new_iar)
+                    db.session.flush()
                     print(new_iar.Id)
                     self.updateRemarks(new_iar.Id, args)
-                    db.session.flush()
+                    
                 
                 
 
