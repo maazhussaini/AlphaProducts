3390d5c29b6a8d71ca706b0e3d4c9ebaf0c7560e
maazhussaini <skr2570@gmail.com>
2024-10-02 10:45:07 +0200

    test

--- HRIS_apis/resources/resources.py
+++ HRIS_apis/resources/resources.py
@@ -6143,7 +6143,7 @@ class EmployeeCreationResource(Resource):
                 logging.info("FILES FOUND")
             # Process form data
             form_data = request.form.to_dict(flat=False)  # Use flat=False for multi-valued keys
-            # logging.info(f"Form data received: {form_data}")
+            logging.info(f"Form data received: {form_data}")
 
             inserted_ids = {}  # To store IDs of inserted records for foreign key relationships
             file_data = {}
